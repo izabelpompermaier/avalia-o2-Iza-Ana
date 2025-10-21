ce60c56fe36121088079943e616b2018233d538f
izabel <izabelpompermaierdaltio@gmail.com>
2025-10-21 09:33:14 -0300

    questão 4 finalizada

--- questão 3/questão 3/Program.cs
+++ questão 3/questão 3/Program.cs
@@ -1,46 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-        int votosCandidatoA = 0;
-        int votosCandidatoB = 0;
-        int votosCandidatoC = 0;
-        int votosNulos = 0;
-        while (true)
-        {
-            Console.Write("\nDigite seu voto:");
-            Console.Write("\n1 para Candidato A");
-            Console.Write("\n2 para Candidato B");
-            Console.Write("\n3 para Candidato C");
-            Console.Write("\n9 para Voto Nulo");
-            Console.Write("\n0 para encerrar\n");
-            int voto;
-            bool parseSuccess = int.TryParse(Console.ReadLine(), out voto);
-            if (!parseSuccess)
-            {
-                Console.WriteLine("Entrada inválida. Por favor, digite um número.");
-                continue;
-            }
-            if (voto == 0)
-            {
-                break;
-            }
-            switch (voto)
-            {
-                case 1:
-                    votosCandidatoA++;
-                    break;
-                case 2:
-                    votosCandidatoB++;
-                    break;
-                case 3:
-                    votosCandidatoC++;
-                    break;
-                case 9:
-                    votosNulos++;
-                    break;
-                default:
-                    Console.WriteLine("Voto inválido. Por favor, digite um número válido.");
-                    break;
-            }
-        }
+        
+int votosCandidatoA = 0;
+int votosCandidatoB = 0;
+int votosCandidatoC = 0;
+int votosNulos = 0;
+while (true) 
+{
+   Console.Write("\nDigite seu voto:");
+   Console.Write("\n1 para Candidato A");
+   Console.Write("\n2 para Candidato B");
+   Console.Write("\n3 para Candidato C");
+   Console.Write("\n9 para Voto Nulo");
+   Console.Write("\n0 para encerrar\n");
+   int voto;
+   bool parseSuccess = int.TryParse(Console.ReadLine(), out voto);
+   if (!parseSuccess)
+   {
+       Console.WriteLine("Entrada inválida. Por favor, digite um número.");
+       continue;
+   }
+   if (voto == 0)
+   {
+        break;
+   }
+    switch (voto)
+    {
+        case 1:
+            votosCandidatoA++;
+            break;
+        case 2:
+            votosCandidatoB++;
+            break;
+        case 3:
+            votosCandidatoC++;
+            break;
+        case 9:
+            votosNulos++;
+            break;
+        default:
+            Console.WriteLine("Voto inválido. Por favor, digite um número válido.");
+            break;
+    }
+}
         Console.WriteLine($"Total de votos para o Candidato A: {votosCandidatoA}");
         Console.WriteLine($"Total de votos para o Candidato B: {votosCandidatoB}");
         Console.WriteLine($"Total de votos para o Candidato C: {votosCandidatoC}");
---
+++ questão 4/questão 4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] nomes = { "Ana", "Izabel", "Lívia", "Gabriel", "Pedro" };
+Console.Write("Digite um nome para pesquisar: ");
+string nomePesquisa = Console.ReadLine();
+bool encontrado = false;
+foreach (string nome in nomes)
+{
+    if (nome.Equals(nomePesquisa, StringComparison.OrdinalIgnoreCase))
+    {
+        encontrado = true;
+        break; 
+    }
+}
+if (encontrado)
+{
+    Console.WriteLine($"O nome {nomePesquisa} foi encontrado no vetor.");
+}
+else
+{
+    Console.WriteLine($"O nome {nomePesquisa} não está no vetor.");
+}
