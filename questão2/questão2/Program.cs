25c65d37c3a5c32230b694377ebb16678f8fc118
izabel <izabelpompermaierdaltio@gmail.com>
2025-10-21 08:48:42 -0300

    questão 2 finalizada

---
+++ questão2/questão2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("digite o número inicial");
+int start = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite um número final");
+int end = int.Parse(Console.ReadLine());
+for (int i = start; i <= end; i++)
+{
+    Console.WriteLine($"\n Tabuada do {i}:");
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+}
